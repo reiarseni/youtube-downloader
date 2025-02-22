e3ef06bee1c41cfbe04c668f63ab48ef42a9ef0d
reiarseni <reiarseni@gmail.com>
2025-02-22 03:22:39 -0500

    Change width of main windows

--- main.py
+++ main.py
@@ -373,6 +373,6 @@ class MainWindow(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.resize(800, 600)
+    window.resize(600, 600)
     window.show()
     sys.exit(app.exec_())
